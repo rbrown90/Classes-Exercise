c94645103db09f19bdaa271f040597125087868b
Rakin Brown <rakinbrown38@gmail.com>
2023-04-26 12:51:15 -0500

    myCar update

---
+++ Classes-Exercise/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Classes_Exercise
+{
+	public class Car
+	{
+		public Car()
+		{
+		}
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+		public Car( string make, string model, int year)
+		{
+			Make = make;
+			Model = model;
+			Year = year;
+		}
+    }
+}
---
+++ Classes-Exercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Classes_Exercise;
+
+namespace ClassExample // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCar = new Car();
+            myCar.Make = "Porsche";
+            myCar.Model = "911";
+            myCar.Year = 2023;
+
+
+            var carList = new List<Car>() { myCar, myCar, myCar, myCar };
+            Console.WriteLine(carList.Capacity);
+        }
+    }
+}
